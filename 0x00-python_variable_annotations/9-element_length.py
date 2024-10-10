963ba5349976801c104e29e9a6613bb378229393
MutabPato <patrickmtbz@gmail.com>
2024-10-10 19:27:34 +0000

    Iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,6 @@ from typing import List, Union, Tuple, Callable, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Duck typing
+    an iterable object """
     return [(i, len(i)) for i in lst]
